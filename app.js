682a5bd4d9ad76e041ebc49be14c12c20a5ec8b4
jason00111 <emberleyjason@gmail.com>
2017-03-10 11:38:08 -0800

    backtrack... form works

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-var app = express()
+const app = express()
 
 app.use(cookieParser())
+app.use(bodyParser())
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
   console.log('Cookies:', req.cookies)
 })
 
+app.post('/', function (req, res) {
+  res.send('your username is ' + req.body.username)
+})
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000!')
 })
