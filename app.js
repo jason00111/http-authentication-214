6ad6cbbd59a8a26b085fdfab3845d8fb596bce22
jason00111 <emberleyjason@gmail.com>
2017-03-10 14:59:33 -0800

    Stage 1 complete

--- app.js
+++ app.js
@@ -5,15 +5,40 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(cookieParser())
-app.use(bodyParser())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const getUsernameHtml =
+`<form method="post" action="/">
+  <label>Enter username:</label>
+  <input type="text" name="username">
+  <input type="submit" value="Send">
+</form>`
+
+const clearUsernameHtml =
+`<form method="post" action="/clear">
+  <input type="submit" value="Clear Username">
+</form>`
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
-  console.log('Cookies:', req.cookies)
+  if ('username' in req.cookies) {
+    res.type('html')
+    res.send(`<p>Your username is</p><pre>${req.cookies.username}</pre>` + clearUsernameHtml)
+  } else {
+    res.type('html')
+    res.send(getUsernameHtml)
+  }
 })
 
 app.post('/', function (req, res) {
-  res.send('your username is ' + req.body.username)
+  res.cookie('username', req.body.username)
+  res.type('html')
+  res.send(`<p>Your username is</p><pre>${req.body.username}</pre>` + clearUsernameHtml)
+})
+
+app.post('/clear', function (req, res) {
+  res.clearCookie('username')
+  res.type('html')
+  res.send(getUsernameHtml)
 })
 
 app.listen(3000, function () {
