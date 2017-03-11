653f432d0dca11cc070c80bae31d429babf1d8ab
jason00111 <emberleyjason@gmail.com>
2017-03-10 17:02:34 -0800

    functions req.setSessionKey etc in use

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
 
 function sessionMiddleware (req, res, next) {
-  if ('session' in req.cookies) {
-    req.session = JSON.parse(req.cookies.session)
-  } else {
-    req.session = {}
+  if (!req.setSession) {
+    req.setSession = function (sessionObject) {
+      req._session = sessionObject
+    }
+    req.setSessionKey = function (key, value) {
+      req._session[key] = value
+      res.cookie('session', JSON.stringify(req._session))
+    }
+    req.getSessionKey = function (key) {
+      return req._session[key]
+    }
+    req.resetSession = function () {
+      req._session = {}
+      res.clearCookie('session')
+    }
   }
+
+  if ('session' in req.cookies) {
+    req.setSession(JSON.parse(req.cookies.session))
+  } else {
+    req.resetSession()
+  }
+
   next()
 }
 
@@ -31,9 +49,9 @@ const clearUsernameHtml =
 </form>`
 
 app.get('/', function (req, res) {
-  if ('username' in req.session) {
+  if (req.getSessionKey('username')) {
     res.type('html')
-    res.send(`<p>Your username is</p><pre>${req.session.username}</pre>` + clearUsernameHtml)
+    res.send(`<p>Your username is</p><pre>${req.getSessionKey('username')}</pre>` + clearUsernameHtml)
   } else {
     res.type('html')
     res.send(getUsernameHtml)
@@ -41,15 +59,13 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  res.cookie('session', JSON.stringify({
-    username: req.body.username
-  }))
+  req.setSessionKey('username', req.body.username)
   res.type('html')
   res.send(`<p>Your username is</p><pre>${req.body.username}</pre>` + clearUsernameHtml)
 })
 
 app.post('/clear', function (req, res) {
-  res.clearCookie('session')
+  req.resetSession()
   res.type('html')
   res.send(getUsernameHtml)
 })
